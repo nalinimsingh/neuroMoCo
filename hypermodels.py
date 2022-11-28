446bf3031e4883a00020a91a14b577e19e3c3208
Nalini <nmsingh@mit.edu>
2022-11-28 10:39:39 -0500

    Use NPA for hypernetwork

--- hypermodels.py
+++ hypermodels.py
@@ -1,3 +1,4 @@
+import math
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.layers import *
@@ -10,13 +11,15 @@ def get_motion_hypernetwork(input_shape = [18], n_layers=6, n_units=256):
     horizontal and vertical translation, and a counterclockwise rotation.  """
 
     hyp_input = tf.keras.Input(shape=input_shape, name='hyp_input')
-    hyp_last = hyp_input
+    hyp_input_encode = Concatenate()([tf.sin(hyp_input*tf.constant(math.pi)),tf.cos(hyp_input*tf.constant(math.pi))])
+    hyp_last = hyp_input_encode
 
     for n in range(n_layers):
         if(n==n_layers-1):
             n_units = 16
         hyp_last = Dense(n_units, activation='relu',
-                         name='hyp_dense_%d' % (n + 1))(hyp_last)
+                         name='hyp_dense_%d' % (n + 1),
+                         bias_initializer = "glorot_uniform")(hyp_last)
 
     hyp_model = tf.keras.Model(inputs=hyp_input, outputs=hyp_last,
                                name='hypernet')
